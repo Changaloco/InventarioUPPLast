36a363e35b9d637d2b4f0fd8fc2b677732b017fd
changaloco <e.alamilla318@gmail.com>
2020-10-28 17:39:51 -0600

    Jwt token -funcionalidad

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-
+const middleware = require('./middlewares');
 const apiTestRouter = require('./api/tests');
 const apiUserRoute = require('./api/users');
 
 
-router.use('/tests',apiTestRouter);
+router.use('/tests',middleware.checkToken,apiTestRouter);
 router.use('/users',apiUserRoute);
 
 
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,7 +2,8 @@ const router  = require('express').Router();
 const bcrypt  = require('bcryptjs');
 const {User} =  require('../../db');
 const {check,validationResult} =require('express-validator');
-
+const moment = require('moment');
+const jwt =require('jwt-simple');
 
 //functions
 router.post('/register',[
@@ -21,7 +22,32 @@ router.post('/register',[
     res.json(user);
 });
 
+router.post('/login',async (req, res) => {
+const user = await User.findOne({where : {email: req.body.email}});
+if (user){
+    const iguales = bcrypt.compareSync(req.body.password, user.password);
+    if(iguales){
+        res.json({sucess : createToken(user)});
 
+    }else{
+        res.json({error: 'Usuario o contrasena invalido' });
+
+    }
+}else{
+    res.json({error: 'Usuario o contrasena invalido'});
+}
+});
+
+const createToken = (user)=>{
+    const payload = {
+        usuarioId : user.id,
+        createdAt : moment().unix(),
+        expiredAt : moment().add(5,'minutes').unix()
+        
+    }
+
+    return jwt.encode(payload,'frase secreta');
+}
 
 //exports 
 
